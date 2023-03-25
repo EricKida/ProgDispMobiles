d4b2d0dfa99060b1af5251d0e72a41d958838dd4
Eric Kida <51220926+EricKida@users.noreply.github.com>
2023-03-25 11:24:03 -0300

    Correção Erro de Exibição - Picker
    O erro consistia em: não exibição do real valor dos picker's sex ("Masculino" / "Feminino") e schooling ("Médio" / "Graduação" / "Pós Graduação" / "MBA").

--- Aula04.1 - Formulario Conta Bancaria/App.js
+++ Aula04.1 - Formulario Conta Bancaria/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, ScrollView, TextInput, Switch, Pressable} from 'react-native';
+import { View, Text, ScrollView, TextInput, Switch, Pressable} from 'react-native';
+import { styles } from './styles';
 import {Picker} from '@react-native-picker/picker';
 import Slider from '@react-native-community/slider';
 
@@ -11,8 +12,8 @@ export default class App extends Component {
       idade: '',
       name: '',
       year: '',
-      sex: 1,
-      schooling: 1,
+      sex: 'Masculino',
+      schooling: 'Medio',
       value: 0,
       brazil: false
     };
@@ -33,24 +34,6 @@ export default class App extends Component {
     this.setState({dataTitle: 'Dados Informados'});
     this.setState({nome: ' '  + this.state.name});
     this.setState({idade: ' ' + this.state.year + ' anos'});
-
-    if(this.state.sex == 1){
-      this.setState({sexo: ' Masculino'});
-    }else if(this.state.sex == 2){
-      this.setState({sexo: ' Feminino'})
-    }
-
-    if(this.state.schooling == 1){
-      this.setState({escolaridade: ' Fundamental'});
-    }else if(this.state.schooling == 2){
-      this.setState({escolaridade: ' Médio Incompleto'})
-    }else if(this.state.schooling == 3){
-      this.setState({escolaridade: ' Médio Completo'})
-    }else if(this.state.schooling == 4){
-      this.setState({escolaridade: ' Superior Incompleto'})
-    }else if(this.state.schooling == 5){
-      this.setState({escolaridade: ' Superior Completo'})
-    }
 
     this.setState({valor: ' R$' + Math.round(this.state.value) + ',00'});
 
@@ -91,22 +74,25 @@ export default class App extends Component {
         <View style={styles.divInput}>
           <Text style={styles.titleInput}>Sexo:   </Text>
           <Picker style={styles.inputType2}
-          onValueChange={ (itemLabel) => this.setState({sex: itemLabel}) }
+            selectedValue={this.state.sex}
+            onValueChange={ (itemValue, itemIndex) => this.setState({sex: itemValue}) }
           >
-            <Picker.Item key={1} value={1} label="Masculino" />
-            <Picker.Item key={2} value={2} label="Feminino" />
+            <Picker.Item key={1} value='Masculino' label="Masculino" />
+            <Picker.Item key={2} value='Feminino' label="Feminino" />
           </Picker>
         </View>
 
         <View style={styles.divInput}>
           <Text style={styles.titleInput}>Escolaridade: </Text>
           <Picker style={styles.inputType2}
-           onValueChange={(itemLabel) => this.setState({schooling: itemLabel})}>
-            <Picker.Item key={1} value={1} label="Fundamental" />
-            <Picker.Item key={2} value={2} label="Médio Incompleto" />
-            <Picker.Item key={3} value={3} label="Médio Completo" />
-            <Picker.Item key={4} value={4} label="Superior Incompleto" />
-            <Picker.Item key={5} value={5} label="Superior Completo" />
+            selectedValue={this.state.schooling}
+            onValueChange={ (itemValue, itemIndex) => this.setState({schooling: itemValue}) }
+          >
+
+            <Picker.Item key={1} value='Médio' label="Médio" />
+            <Picker.Item key={2} value='Graduação' label="Graduação" />
+            <Picker.Item key={3} value='Pós Graduação' label="Pós Graduação" />
+            <Picker.Item key={4} value='MBA' label="MBA" />
           </Picker>
         </View>
 
@@ -142,32 +128,32 @@ export default class App extends Component {
           <Text style={styles.titleData}> Dados Informados </Text>
 
           <Text style={styles.topic}>
-            <Text style={styles.textTopic}>Nome:</Text>
+            <Text style={styles.textTopic}>Nome: </Text>
             <Text style={styles.textData}>{this.state.nome}</Text>
           </Text>
 
           <Text style={styles.topic}>
-            <Text style={styles.textTopic}>Idade:</Text>
+            <Text style={styles.textTopic}>Idade: </Text>
             <Text style={styles.textData}>{this.state.idade}</Text>
           </Text>
 
           <Text style={styles.topic}>
-            <Text style={styles.textTopic}>Sexo:</Text>
-            <Text style={styles.textData}>{this.state.sexo}</Text>
+            <Text style={styles.textTopic}>Sexo: </Text>
+            <Text style={styles.textData}>{this.state.sex}</Text>
           </Text>
 
           <Text style={styles.topic}>
-            <Text style={styles.textTopic}>Escolaridade:</Text>
-            <Text style={styles.textData}>{this.state.escolaridade}</Text>
+            <Text style={styles.textTopic}>Escolaridade: </Text>
+            <Text style={styles.textData}>{this.state.schooling}</Text>
           </Text>
 
           <Text style={styles.topic}>
-            <Text style={styles.textTopic}>Limite:</Text>
+            <Text style={styles.textTopic}>Limite: </Text>
             <Text style={styles.textData}>{this.state.valor}</Text>
           </Text>
 
           <Text style={styles.topic}>
-            <Text style={styles.textTopic}>Brasileiro:</Text>
+            <Text style={styles.textTopic}>Brasileiro: </Text>
             <Text style={styles.textData}>{this.state.nacionalidade}</Text>
           </Text>
 
@@ -178,95 +164,4 @@ export default class App extends Component {
  }
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#121212',
-    padding: 1,
-  },
-  titlePage: {
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
-    padding: 50,
-  },
-  titleInput: {
-    color: '#fff',
-    marginTop: 10
-  },
-  divInput: {
-    flexDirection: 'row',
-    padding: 10,
-  },
-  inputType1: {
-    backgroundColor: '#fff',
-    padding: 4,
-    marginTop: 3,
-    width: 300,
-    borderRadius: 4,
-    marginLeft: 10,
-  },
-  inputType2: {
-    backgroundColor: '#fff',
-    padding: 4,
-    marginTop: 3,
-    width: 200,
-    borderRadius: 4,
-    marginLeft: 10,
-  },
-  limitInput: {
-    color: '#fff',
-    fontWeight: 'bold',
-    marginTop: 10,
-  },
-  sliderInput:{
-    padding: 10
-  },
-  switchInput: {
-    marginTop: -4,
-  },
-  button: {
-    width: 200,
-    alignItems: 'center',
-    backgroundColor: '#007AFF',
-    padding: 16,
-    borderRadius: 4,
-  },
-  buttonText: {
-    textAlign: 'center',
-    color: '#fff',
-  },
-  viewButton: {
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textTopic: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginLeft: 20
-  },
-  textData: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  titleData: {
-    color: '#fff',
-    textAlign: 'center',
-    fontSize: 20,
-    fontWeight: 'bold',
-    padding: 20,
-  },
-  dataView: {
-    marginTop: 25,
-    borderRadius: 20,
-  },
-  topic: {
-    marginTop: 5,
-    padding: 15
-  },
-});
 
