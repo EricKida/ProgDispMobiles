ff856f459800dd007d1b426925293f7517fc8dcc
Eric Kida <51220926+EricKida@users.noreply.github.com>
2023-03-31 16:29:30 -0300

    Adição styles.js

---
+++ Aula02.1 - Contador/styles.js
@@ -0,0 +1,64 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    padding: 1,
+  },
+  contadorView: {
+    marginTop: 10,
+    alignSelf: 'center',
+    backgroundColor: '#1C1C1C',
+    width: 200,
+    height: 200,
+    borderRadius: 30,
+    borderWidth: 5,
+    borderColor: '#1C1C1C',
+    marginBottom: 20
+  },
+  count: {
+    marginTop: 35,
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 80,
+  },
+  op: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+  },
+  buttonInc: {
+    width: 100,
+    height: 50,
+    backgroundColor: 'green',
+    marginRight: 15,
+    borderRadius: 10
+  },
+  Inc: {
+    color: '#fff',
+    fontSize: 31,
+    textAlign: 'center'
+  },
+  buttonDec: {
+    width: 100,
+    height: 50,
+    color: '#fff',
+    backgroundColor: 'red',
+    borderRadius: 10
+  },
+  Dec: {
+    color: '#fff',
+    fontSize: 31,
+    textAlign: 'center'
+  },
+  titlePage: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    padding: 40,
+  },
+});
+
+export {styles};
