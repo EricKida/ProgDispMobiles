20733c838c6fdf7edfa4b0e01a0f1cab3ca15cd9
Eric Kida <51220926+EricKida@users.noreply.github.com>
2023-04-02 03:43:18 -0300

    Alteração para utilização de Hooks - useState

--- Aula03.2 - Cálculo IMC/App.js
+++ Aula03.2 - Cálculo IMC/App.js
@@ -1,57 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Text, View, Pressable, TextInput, Image } from 'react-native';
 import { styles } from './styles';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      resultado: '',
-      result: '',
-      altura: '',
-      peso: '',
-    };
+export default function App() {
+  const [peso, setPeso] = useState('0');
+  const [altura, setAltura] = useState('0');
+  const [resultado, setResultado] = useState('');
+  const [imc, setImc] = useState('');
+  const [colorTxt, setColorTxt] = useState('');
 
-    this.calcular = this.calcular.bind(this);
-  }
+    function calcular() {
+      if (peso === '' || altura === '') {
+        alert('Opa! Faltou alguma informação, verifique novamente se os campos foram preenchidos.')
+          setResultado('Valores Insuficientes');
+          setColorTxt('#FFF');
+          setImc('Nível IMC: 0');
+      } else {
+        const imcTemp = peso / (altura * altura);
+        setImc('Nível IMC: ' + imcTemp.toFixed(1));
 
-  calcular(){
-    if ( (this.state.peso === '') || (this.state.altura === '') ){
-      alert('Opa! Faltou alguma informação, verifique novamente se os campos foram preenchidos.')
-      return;
+        if (imcTemp < 18.5) {
+          setResultado('Abaixo do peso');
+          setColorTxt('#FFC0CB');
+        } else if (imcTemp <= 24.9) {
+          setResultado('Peso Normal');
+          setColorTxt('#7CFC00');
+        } else if (imcTemp <= 29.9) {
+          setResultado('Sobrepeso');
+          setColorTxt('#FFA07A');
+        } else if (imcTemp <= 34.9) {
+          setResultado('Obesidade Grau 1');
+          setColorTxt('#FF8C00');
+        } else if (imcTemp <= 39.9) {
+          setResultado('Obesidade Grau 2');
+          setColorTxt('#FF4500');
+        } else if (imcTemp > 39.9) {
+          setResultado('Obesidade Grau 3');
+          setColorTxt('#FF0000');
+        }
+      }
     }
 
-    res = (this.state.peso / (this.state.altura * this.state.altura)).toFixed(1)
-
-    if(res < 18.5){
-      this.setState({resultado: 'Abaixo do peso'});
-      this.setState({colorTxt: '#ffe933'});
-    }
-    else if(res <= 24.9){
-      this.setState({resultado: 'Peso Normal'});
-      this.setState({colorTxt: '#6cff00'});
-    }
-    else if(res <= 29.9){
-      this.setState({resultado: 'Sobrepeso'});
-      this.setState({colorTxt: '#ff7e00'});
-    }
-    else if(res <= 34.9){
-      this.setState({resultado: 'Obesidade Grau 1'});
-      this.setState({colorTxt: '#ff4e00'});
-    }
-    else if(res <= 39.9){
-      this.setState({resultado: 'Obesidade Grau 2'});
-      this.setState({colorTxt: '#ff3600'});
-
-    }else{
-      this.setState({resultado: 'Obesidade Grau 3'});
-      this.setState({colorTxt: '#ff0000'});
-    }
-    this.setState({imc: 'Nível IMC: ' + res});
-  }
-
-  render() {
-    let color_Font = this.state.colorTxt;
     return (
       <View style={styles.container}>
         <View style={styles.viewTitle}>
@@ -66,7 +55,7 @@ class App extends Component {
           <TextInput
             style={styles.input}
             placeholder="Insira seu peso"
-            onChangeText={(valor) => this.setState({ peso: valor })}
+            onChangeText={(text) => setPeso(text)}
             keyboardType="numeric"
           />
         </View>
@@ -76,23 +65,22 @@ class App extends Component {
           <TextInput
             style={styles.input}
             placeholder="Insira sua altura"
-            onChangeText={(valor) => this.setState({ altura: valor })}
+            onChangeText={(text) => setAltura(text)}
             keyboardType="numeric"
           />
         </View>
 
-        <Pressable style={styles.button} onPress={this.calcular}>
+        <Pressable style={styles.button} onPress={calcular}>
           <Text style={styles.text}>Verificar</Text>
         </Pressable>
 
         <View style={styles.resultado}>
           <Text style={styles.titleResult}>Classificação</Text>
-          <Text style={[styles.textClassification, { color: color_Font}]}>{this.state.resultado}</Text>
-          <Text style={styles.textResult}>{this.state.imc}</Text>
+          <Text style={[styles.textClassification, { color: colorTxt}]}>{resultado}</Text>
+          <Text style={styles.textResult}>{imc}</Text>
         </View>
       </View>
     );
   }
-}
 
-export default App;
+
